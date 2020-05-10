25b31507c189505c63413c6c0fd2b0b90fa61e38
Nischal Hada <hadanischal@gmail.com>
2020-05-10 21:20:11 +1000

    [Search textfield] implemented NewsListWidget and added  Search TextBox And TextEditingController

--- lib/views/NewsListView.dart
+++ lib/views/NewsListView.dart
@@ -1,50 +1,60 @@
 import 'package:flutter/material.dart';
 import 'package:news_top_headlines/viewmodels/NewsArticleListViewModel.dart';
+import 'package:news_top_headlines/widgets/NewsListWidget.dart';
 import 'package:provider/provider.dart';
 
-/*
 class NewsListView extends StatefulWidget {
-  @override 
-  _NewsListState createState() => _NewsListState(); 
+  @override
+  _NewsListViewState createState() => _NewsListViewState();
 }
 
-//Second way
-class _NewsListState extends State<NewsListView> {
+class _NewsListViewState extends State<NewsListView> {
+  final _controller = TextEditingController();
 
   @override
   void initState() {
     super.initState();
 
-    Provider.of<NewsArticleListViewModel>(context,listen: false).populateTopHeadlines();
+    Provider.of<NewsArticleListViewModel>(context, listen: false)
+        .populateTopHeadlines();
   }
-*/
-
-/// One way
-class NewsListView extends StatelessWidget {
-  // NewsArticleListViewModel _viewModel = NewsArticleListViewModel();
-
-  // NewsListView() {
-  //   _viewModel.populateTopHeadlines;
-  // }
 
   @override
   Widget build(BuildContext context) {
     final vm = Provider.of<NewsArticleListViewModel>(context);
+
+    var searchIcon = Icon(Icons.search);
+
+    var sarchTextField = TextField(
+      controller: _controller,
+      onSubmitted: (_) {
+        // TODO: fetch all news
+      },
+      decoration: InputDecoration(
+        labelText: "Search",
+        icon: Padding(
+          padding: const EdgeInsets.all(10.0),
+          child: searchIcon,
+        ),
+        suffixIcon: IconButton(
+          icon: Icon(Icons.clear), 
+          onPressed: () {
+            _controller.clear();
+          }
+          ),
+      ),
+    );
+
     return Scaffold(
         appBar: AppBar(title: Text("Top News")),
-        body: ListView.builder(
-          itemCount: vm.articles.length,
-          itemBuilder: (context, index) {
-
-            final article = vm.articles[index];
-            var newsImage = article.imageURL != null ? Image.network(article.imageURL) : Image.asset("assets/news-placeholder.png");
-            var imageContainer = Container(width: 100, height: 100, child: newsImage);
-
-            return ListTile(
-              leading: imageContainer,
-              title: Text(article.title),
-            );
-          },
-        ));
+        body: Column(children: <Widget>[
+          sarchTextField,
+          Expanded(
+            child: NewsListWidget(
+              articles: vm.articles,
+              onSelected: (_) {},
+            ),
+          )
+        ]));
   }
 }
